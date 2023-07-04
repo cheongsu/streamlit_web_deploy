f4850321f47ae37f1c615d8a49d4c006b08aceb4
cheongsu <cheong1282@gmail.com>
2023-07-04 17:52:10 +0900

    txt edit

--- speech_to_text.py
+++ speech_to_text.py
@@ -28,27 +28,27 @@
 
 
 
-import os
-import numpy as np
-import streamlit as st
-from io import BytesIO
-import streamlit.components.v1 as components
-from st_custom_components import st_audiorec
-wav_audio_data = st_audiorec()
-import speech_recognition as sr
-import scipy.io.wavfile as wavf
+# import os
+# import numpy as np
+# import streamlit as st
+# from io import BytesIO
+# import streamlit.components.v1 as components
+# from st_custom_components import st_audiorec
+# wav_audio_data = st_audiorec()
+# import speech_recognition as sr
+# import scipy.io.wavfile as wavf
 
 
 
 
-out_f = 'output.wav'
-wavf.write(out_f, sr, wav_audio_data)
-r = sr.Recognizer()
-with sr.AudioFile(out_f) as source:
-    audio = r.record(source)  # 전체 audio file 읽기
+# out_f = 'output.wav'
+# wavf.write(out_f, sr, wav_audio_data)
+# r = sr.Recognizer()
+# with sr.AudioFile(out_f) as source:
+#     audio = r.record(source)  # 전체 audio file 읽기
 
-input_text = r.recognize_google(audio, language='ko')
-print(input_text)
+# input_text = r.recognize_google(audio, language='ko')
+# print(input_text)
 # r = sr.Recognizer()
 # with sr.AudioFile(wav_audio_data) as source:
 #     audio = r.record(source)  # 전체 audio file 읽기
