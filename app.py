e755b00e40ec2b309831199fc6835ee7cf90f389
cheongsu <cheong1282@gmail.com>
2023-07-04 17:38:52 +0900

    voice 추가

--- app.py
+++ app.py
@@ -313,44 +313,6 @@
 #     """))
 # st.bokeh_chart(tts_button)
 
-# import streamlit as st
-# from bokeh.models.widgets import Button
-# from bokeh.models import CustomJS
-# from streamlit_bokeh_events import streamlit_bokeh_events
-
-# stt_button = Button(label="Speak", width=100)
-
-# stt_button.js_on_event("button_click", CustomJS(code="""
-#     var recognition = new webkitSpeechRecognition();
-#     recognition.continuous = true;
-#     recognition.interimResults = true;
-
-#     recognition.onresult = function (e) {
-#         var value = "";
-#         for (var i = e.resultIndex; i < e.results.length; ++i) {
-#             if (e.results[i].isFinal) {
-#                 value += e.results[i][0].transcript;
-#             }
-#         }
-#         if ( value != "") {
-#             document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-#         }
-#     }
-#     recognition.start();
-#     """))
-
-# result = streamlit_bokeh_events(
-#     stt_button,
-#     events="GET_TEXT",
-#     key="listen",
-#     refresh_on_update=False,
-#     override_height=75,
-#     debounce_time=0)
-
-# if result:
-#     if "GET_TEXT" in result:
-#         st.write(result.get("GET_TEXT"))
-
 # app.py
 
 import streamlit as st
@@ -391,3 +353,42 @@ chart_data = pd.DataFrame(
 
 # 컬럼 나머지 부분에 라인차트 생성
 cols[2].line_chart(chart_data)
+
+
+import streamlit as st
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+
+stt_button = Button(label="Speak", width=100)
+
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_TEXT" in result:
+        st.write(result.get("GET_TEXT"))
